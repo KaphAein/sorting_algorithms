25f819ebcf49ccc140b21a851549321375d3787e
kaphaein <cheriffadaly@github.com>
2023-12-17 04:39:15 -0800

    “o”

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, tmp;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+		{
+			if (array[j] > array[j + 1] && array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+	print_array(array, size);
+}
