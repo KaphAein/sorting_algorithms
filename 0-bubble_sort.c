9b5f7187116c3d2a6f38c37780ee5f87ccccff97
kaphaein <cheriffadaly@github.com>
2023-12-17 04:56:48 -0800

    “o”

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,7 +17,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1] && array[j + 1])
 			{
-				tmp = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 			}
